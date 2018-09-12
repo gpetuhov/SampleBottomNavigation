4b69256bd4a84a5e9979be0526ca67e28f5cdb26
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-09-12 11:38:50 +0300

    Tie nav controller to bottom navigation bar

--- app/src/main/java/com/gpetuhov/android/samplebottomnavigation/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplebottomnavigation/MainActivity.kt
@@ -2,11 +2,26 @@ package com.gpetuhov.android.samplebottomnavigation
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find NavController
+        navController = findNavController(R.id.nav_host)
+
+        // Tie NavHostFragment to bottom navigation bar
+        navigationView.setupWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp() = navController.navigateUp()
 }
